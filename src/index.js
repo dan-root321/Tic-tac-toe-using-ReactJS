9cd04213ce4c82febc16a461d36ead6f611d3e92
#Kali <brucaldan@gmail.com>
2023-07-24 13:15:09 +0800

    First Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react";
+import "./styles.css";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
